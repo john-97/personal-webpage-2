a2c17922d562e1159059921265bc2bd8c8f07a3e
Jonathan Chow <john@xen.net>
2019-07-15 13:59:06 +0800

    welcome page bg

--- client/components/landing.js
+++ client/components/landing.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Header from './common/header'
+import Footer from './common/footer'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className="landing-page">
+      <Header />
+      <div className="landing-page-body">
+        <div className="landing-page-body-welcome">WELCOME</div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
